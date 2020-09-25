fac6a4b2a460c4b0bf2006c3efaa74c0362bdf2a
Daniel Ramos <danielramosacosta@hotmail.com>
2020-09-25 19:48:53 +0100

    Update nextjs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-  exportTrailingSlash: true,
-  exportPathMap: function() {
-    return {
-      '/': { page: '/' },
-      '/terms-and-conditions': { page: '/terms-and-conditions' },
-      '/privacy-policy': { page: '/privacy-policy' },
-    };
-  }
+  trailingSlash: true,
+  basePath: '/acid-labs/kairos-wallet/web',
+  assetPrefix: '/acid-labs/kairos-wallet/web/',
 };
