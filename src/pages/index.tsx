4dced6ba142b942b6793dd6ee64001cefb2c82bf
Daniel Ramos <danielramosacosta@hotmail.com>
2020-09-25 17:54:19 +0100

    finish

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,25 +15,22 @@ export default function Index() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div className={styles.container}>
-        <img className={styles.background} src="/pattern-bg.svg" alt="" />
+        <img className={styles.circle} src="/circles/circle1.svg" alt="" />
         <img className={styles.cornerLogo} src="/logo.svg" alt="" />
         <div className={styles.mobile}>
           <img src="/mobile-mockup.png" alt="mobile-with-kairos" />
         </div>
         <main className={styles.main}>
           <img className={styles.logo} src="/logo.svg" alt="Kairos Logo" />
-          <Title>Productos básicos cerca de ti</Title>
+          <Title>Kairos Wallet</Title>
           <Paragraph className={styles.paragraph}>
-            Aquí podrás encontrar productos de primera necesidad (leche, huevos, pan, azúcar, papel
-            higiénico, productos de limpieza, alcohol etílico, termómetros, gel desinfectante,
-            analgésicos…) cerca de ti. Además, también podrás ver las franjas horarias de mayor
-            afluencia en los supermercados y farmacias de tu entorno, para ahorrar tiempo y evitar
-            colas innecesarias.
+          A DeFi non custodial Ethereum Blockchain wallet
           </Paragraph>
-          <Paragraph>
-            Mediante tu contribución ayudaremos a optimizar la movilidad ciudadana, evitar
-            masificaciones, y distribuir la demanda local para que siempre podamos acceder a
-            productos de primera necesidad.
+          <Paragraph className={styles.paragraph}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea sapiente minima ullam iure doloribus quas tenetur laboriosam laudantium cupiditate maxime, adipisci error doloremque quibusdam quos quis dignissimos asperiores facilis dolorum!
+          </Paragraph>
+          <Paragraph className={styles.paragraph}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea sapiente minima ullam iure doloribus quas tenetur laboriosam laudantium cupiditate maxime, adipisci error doloremque quibusdam quos quis dignissimos asperiores facilis dolorum!
           </Paragraph>
         </main>
         <div className={styles.markets}>
