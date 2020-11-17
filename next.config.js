58efe2508c33a5a71946d53fbd5b2eef2dd9a2f7
Abel Garcia Dorta <abelgarcia@acidtango.com>
2020-11-17 12:51:05 +0000

    fix: assetPrefix and basePath are different on github pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   trailingSlash: true,
-  basePath: '/acid-labs/kairos-wallet/web',
-  assetPrefix: '/acid-labs/kairos-wallet/web/',
+  basePath: '/web',
+  assetPrefix: '/web',
 };
