6910edd144d16c051d0c6141eeb1226ce4c3b498
Abel Garcia Dorta <abelgarcia@acidtango.com>
2022-01-26 15:52:35 +0000

    fix: react-markdown changed from source to children

--- src/component/Document.tsx
+++ src/component/Document.tsx
@@ -7,5 +7,5 @@ import styles from "./Document.module.css"
 type DocumentProps = { markdown: string }
 
 export const Document: FC<DocumentProps> = ({ markdown }) => (
-  <ReactMarkdown source={markdown} className={styles.document} />
+  <ReactMarkdown children={markdown} className={styles.document} />
 )
